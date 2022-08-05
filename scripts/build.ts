1a4a492545c4bb0a508895a865ad872fc28984d6
Ansible2 <afoshark@yahoo.com>
2022-08-05 16:53:16 -0600

    Added more build process

--- scripts/build.ts
+++ scripts/build.ts
@@ -12,9 +12,30 @@ const sqfConfigFilePathOut = `${outConfigFolderPath}/sqf.configuration.json`;
 
 // copy configuration.json's to .out folder
 const build = async () => {
-	const copyExtConfig = fsPromises.copyFile(extConfigFilePath,extConfigFilePathOut);
-	const copySqfConfig = fsPromises.copyFile(sqfConfigFilePath,sqfConfigFilePathOut);
+	console.log("\nExecuting build process...");
+	
+	const outDirectoryExitsts: boolean = fs.existsSync(outFolderPath);
+	if (!outDirectoryExitsts) {
+		console.log("Out directory does not exist...");
+		console.log(`Creating at: ${outFolderPath}`);
 
+		fs.mkdirSync(outFolderPath);
+	}
+
+	const outConfigsDirectoryExitsts: boolean = fs.existsSync(outConfigFolderPath);
+	if (!outConfigsDirectoryExitsts) {
+		console.log("Out configs directory does not exist...");
+		console.log(`Creating at: ${outConfigFolderPath}`);
+		
+		fs.mkdirSync(outConfigFolderPath);
+	}
+	
+	console.log();
+
+	const copyExtConfig: Promise<void> = fsPromises.copyFile(extConfigFilePath,extConfigFilePathOut);
+	console.log("Copying .ext language configuration json file...");
+	const copySqfConfig: Promise<void> = fsPromises.copyFile(sqfConfigFilePath,sqfConfigFilePathOut);
+	console.log("Copying .sqf language configuration json file...");
 	
 	await Promise.all([copyExtConfig, copySqfConfig])
 }
