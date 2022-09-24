593f563a423a85fd4db378e0889f0cddbdd25c94
Ansible2 <afoshark@yahoo.com>
2022-09-24 15:13:02 -0600

    Abstracted wiki parsers into class

---
+++ scripts/BIS Wiki Scraper/BISWikiParser.humble.ts
@@ -0,0 +1,347 @@
+interface ParsedSyntax {
+    commandName: string;
+    leftArgType?: string | string[];
+    rightArgType?: string | string[];
+    returnType: string;
+    type: SyntaxType;
+}
+
+enum SyntaxMatchDifference {
+    leftArg,
+    rightArg,
+    NoMatch,
+}
+
+enum SyntaxType {
+    binary = "SQFSyntaxType.BinaryOperator",
+    unary = "SQFSyntaxType.UnaryOperator",
+    nular = "SQFSyntaxType.NularOperator",
+}
+
+
+// const regex = /(\|(\s*|\S*)\=).*?(?=(\|(\s*|\S*)\=)|(?=}}$)|(?=$))/gi;
+// group 1: Actual syntax for an entry
+// - match starts with a '|'
+// - followed by any amount of whitespace OR any amount of non-whitespace characters
+// - and lastly an '=' sign
+
+// '.*?' matches any character between zero and unlimited times, as few times as possible, expanding as needed (lazy)
+
+// group 2:
+// - Positive Lookahead (?=) Assert that the following below matches but DO NOT include it in the match
+// - the rest is asserting that the match is followed by a string that matches any of these:
+// 		- the same pattern in group 1
+// 		- A set of closing '}}'
+// 		- The end of the whole string the regex is being used on
+
+
+class TextInterpreter {
+    private static readonly wikiTypeToDataTypeMap = {
+        NUMBER: "SQFDataType.Number",
+        SCALAR: "SQFDataType.Number",
+        BOOLEAN: "SQFDataType.Bool",
+        BOOL: "SQFDataType.Bool",
+		ARRAY: "SQFDataType.Array",
+		STRING: "SQFDataType.String",
+		NOTHING: "SQFDataType.Nothing",
+		ANY: "SQFDataType.Any",
+		ANYTHING: "SQFDataType.Any",
+		NAMESPACE: "SQFDataType.Namespace",
+		NAN: "SQFDataType.NaN",
+		IF: "SQFDataType.IfType",
+		WHILE: "SQFDataType.WhileType",
+		FOR: "SQFDataType.ForType",
+		SWITCH: "SQFDataType.SwitchType",
+		EXCEPTION: "SQFDataType.Exception",
+		WITH: "SQFDataType.WithType",
+		CODE: "SQFDataType.Code",
+		OBJECT: "SQFDataType.Object",
+		VECTOR: "SQFDataType.Vector",
+		SIDE: "SQFDataType.Side",
+		GROUP: "SQFDataType.Group",
+		TEXT: "SQFDataType.StructuredText",
+		SCRIPT: "SQFDataType.ScriptHandle",
+		CONFIG: "SQFDataType.Config",
+		DISPLAY: "SQFDataType.Display",
+		CONTROL: "SQFDataType.Control",
+		NETOBJECT: "SQFDataType.NetObject",
+		TEAM_MEMBER: "SQFDataType.TeamMember",
+		HASHMAP: "SQFDataType.HashMap",
+		TASK: "SQFDataType.Task",
+		DIARY_RECORD: "SQFDataType.DiaryRecord",
+		LOCATION: "SQFDataType.Location",
+		HASHMAPKEY: "SQFDataType.HashMapKey",
+    };
+    isSyntax(stringToCheck: string): boolean {
+        return !!stringToCheck.match(/^\|s\d*\=/);
+    }
+    isParameter(stringToCheck: string): boolean {
+        return !!stringToCheck.match(/^\|p\d*=/);
+    }
+    isReturnType(stringToCheck: string): boolean {
+        return !!stringToCheck.match(/^\|r\d*=/);
+    }
+    isExample(stringToCheck: string): boolean {
+        return !!stringToCheck.match(/^\|x\d*\=/);
+    }
+    isDescription(stringToCheck: string): boolean {
+        return stringToCheck.startsWith("|descr");
+    }
+    isArgLocality(stringToCheck: string): boolean {
+        return stringToCheck.startsWith("|arg");
+    }
+    isEffectLocality(stringToCheck: string): boolean {
+        return stringToCheck.startsWith("|eff");
+    }
+
+    convertWikiTypeToSQFDataType(unParsedType: string): string {
+        const typeParsed = TextInterpreter.wikiTypeToDataTypeMap[unParsedType.toUpperCase()];
+		if (typeParsed) return typeParsed;
+		throw `convertWikiTypeToSQFDataType: Did not find parse type for: ${unParsedType}`;
+        return "SQFDataType.Empty";
+    }
+}
+
+export class BISWikiParser {
+    private readonly textInterpreter: TextInterpreter;
+    constructor() {
+        this.textInterpreter = new TextInterpreter();
+    }
+
+	parseType(unParsedString: string): string {
+		const typeMatches: RegExpMatchArray | null =
+			unParsedString.match(/\[\[(\w*?)\]\]/);
+		if (!typeMatches) {
+			throw `Could not type in string: ${unParsedString}`;
+		}
+	
+		const parsed = this.textInterpreter.convertWikiTypeToSQFDataType(typeMatches[1]);
+		return parsed;
+	}
+
+	createParsedSyntaxWithParams(
+		inputSyntax: ParsedSyntax,
+		parameters: string[]
+	): ParsedSyntax {
+		switch (parameters.length) {
+			case 0: {
+				inputSyntax.type = SyntaxType.nular;
+				break;
+			}
+			case 1: {
+				inputSyntax.type = SyntaxType.unary;
+				inputSyntax.rightArgType = parameters[0];
+				break;
+			}
+			case 2: {
+				inputSyntax.leftArgType = parameters[0];
+				inputSyntax.rightArgType = parameters[1];
+				inputSyntax.type = SyntaxType.binary;
+				break;
+			}
+			default: {
+				inputSyntax.type = SyntaxType.nular;
+				break;
+			}
+		}
+	
+		return inputSyntax;
+	}
+	
+	parsePageIntoSyntaxes(command: string, page: string): ParsedSyntax[] {
+		const parsedSyntaxes: ParsedSyntax[] = [];
+		try {
+			const regex = /(\|(\s*|\S*)\=).*?(?=(\|(\s*|\S*)\=)|(?=}}$)|(?=$))/gi;
+			const regexMatches: RegExpMatchArray | null = page.match(regex);
+			if (!regexMatches) {
+				throw "No regex matches found";
+			}
+	
+			let currentSyntax: ParsedSyntax | undefined;
+			let parameters: string[] = [];
+			regexMatches.forEach((match: string) => {
+				const matchTrimmed = match.trim();
+				if (this.textInterpreter.isDescription(matchTrimmed)) {
+				} else if (this.textInterpreter.isExample(matchTrimmed)) {
+				} else if (this.textInterpreter.isSyntax(matchTrimmed)) {
+					if (currentSyntax) {
+						const fullSyntax = this.createParsedSyntaxWithParams(
+							currentSyntax,
+							parameters
+						);
+						parsedSyntaxes.push(fullSyntax);
+					}
+	
+					currentSyntax = {
+						commandName: command,
+					} as ParsedSyntax;
+					parameters = [];
+				} else if (this.textInterpreter.isArgLocality(matchTrimmed)) {
+				} else if (this.textInterpreter.isEffectLocality(matchTrimmed)) {
+				} else if (this.textInterpreter.isParameter(matchTrimmed)) {
+					const parameterType: string = this.parseType(matchTrimmed);
+					parameters.push(parameterType);
+				} else if (this.textInterpreter.isReturnType(matchTrimmed)) {
+					const returnType: string = this.parseType(matchTrimmed);
+					currentSyntax!.returnType = returnType;
+				}
+			});
+	
+			if (currentSyntax) {
+				const fullSyntax = this.createParsedSyntaxWithParams(
+					currentSyntax,
+					parameters
+				);
+				parsedSyntaxes.push(fullSyntax);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	
+		return parsedSyntaxes;
+	}
+
+	syntaxMatch(
+		syntax1: ParsedSyntax,
+		syntax2: ParsedSyntax
+	): SyntaxMatchDifference {
+		const type: SyntaxType = syntax1.type;
+		if (
+			type === SyntaxType.nular ||
+			type !== syntax2.type ||
+			syntax1.returnType !== syntax2.returnType
+		)
+			return SyntaxMatchDifference.NoMatch;
+	
+		const leftArgIsDefined: boolean = !!syntax1.leftArgType;
+		const rightArgIsDefined: boolean = !!syntax1.rightArgType;
+		const leftArgIsTheSame = syntax1.leftArgType === syntax2.leftArgType;
+		const rightArgIsTheSame = syntax1.rightArgType === syntax2.rightArgType;
+	
+		if (type === SyntaxType.binary) {
+			if (leftArgIsDefined && !leftArgIsTheSame && rightArgIsTheSame) {
+				return SyntaxMatchDifference.leftArg;
+			} else if (
+				rightArgIsDefined &&
+				!rightArgIsTheSame &&
+				leftArgIsTheSame
+			) {
+				return SyntaxMatchDifference.rightArg;
+			}
+		}
+	
+		if (type === SyntaxType.unary) {
+			if (leftArgIsDefined && !leftArgIsTheSame) {
+				return SyntaxMatchDifference.leftArg;
+			} else if (rightArgIsDefined && !rightArgIsTheSame) {
+				return SyntaxMatchDifference.rightArg;
+			}
+		}
+	
+		return SyntaxMatchDifference.NoMatch;
+	}
+
+	consolidateSyntaxes(command: string,parsedSyntaxes: ParsedSyntax[]): string {
+		const usedIndexes: number[] = [];
+		const consolidatedSyntaxes: ParsedSyntax[] = [];
+		for (let i = 0; i < parsedSyntaxes.length; i++) {
+			if (i in usedIndexes) continue;
+			usedIndexes.push(i);
+			const mainSyntax = parsedSyntaxes[i];
+			
+			for (let j = i + 1; j < parsedSyntaxes.length; j++) {
+				if (j in usedIndexes) continue;
+				const compareSyntax = parsedSyntaxes[j];
+				const syntaxMatchDiff: SyntaxMatchDifference = this.syntaxMatch(mainSyntax,compareSyntax);
+	
+				switch (syntaxMatchDiff) {
+					case SyntaxMatchDifference.NoMatch: {
+						continue;
+					}
+					case SyntaxMatchDifference.leftArg: {
+						const syntaxToAdd = compareSyntax.leftArgType as string;
+						if (Array.isArray(mainSyntax.leftArgType)) {
+							const syntaxAsArray = mainSyntax.leftArgType as string[]
+							mainSyntax.leftArgType = [...syntaxAsArray,syntaxToAdd];
+						} else {
+							mainSyntax.leftArgType = [mainSyntax.leftArgType!,syntaxToAdd];
+						}
+						
+						break;
+					}
+					case SyntaxMatchDifference.rightArg: {
+						const syntaxToAdd = compareSyntax.rightArgType as string;
+						if (Array.isArray(mainSyntax.rightArgType)) {
+							const syntaxAsArray = mainSyntax.rightArgType as string[]
+							mainSyntax.rightArgType = [...syntaxAsArray,syntaxToAdd];
+						} else {
+							mainSyntax.rightArgType = [mainSyntax.rightArgType!,syntaxToAdd];
+						}
+						break;
+					}
+					default:
+						continue;
+				}
+	
+				usedIndexes.push(i);
+			}
+	
+			consolidatedSyntaxes.push(mainSyntax);
+		}
+		
+		let syntaxesAsString: string[] | string = consolidatedSyntaxes.map((syntax: ParsedSyntax) => {
+			const syntaxArray = [
+				'{',
+				`\t\ttype: ${syntax.type},`,
+				`\t\treturnTypes: ${syntax.returnType},`,
+			];
+			
+			if (syntax.leftArgType) {
+				let insertSyntax = syntax.leftArgType;
+				if (Array.isArray(syntax.leftArgType)) {
+					insertSyntax = `[${insertSyntax}]`
+				}
+				syntaxArray.push(`\t\tleftOperandTypes: ${insertSyntax}`);
+			}
+			if (syntax.rightArgType) {
+				let insertSyntax = syntax.rightArgType;
+				if (Array.isArray(syntax.rightArgType)) {
+					insertSyntax = `[${insertSyntax}]`
+				}
+				syntaxArray.push(`\t\trightOperandTypes: ${insertSyntax}`);
+			}
+	
+			syntaxArray.push('\t}',);
+			return syntaxArray.join("\n");
+		});
+	
+		const moreThanOneSyntax = consolidatedSyntaxes.length > 1;
+		if (!moreThanOneSyntax) {
+			syntaxesAsString = syntaxesAsString[0]
+		}
+		const syntaxArray: string[] = [
+			`${command}: {`,
+			`\tsyntaxes: ${syntaxesAsString},`,
+			`\tgrammarType: SQFGrammarType.Command,`,
+			'},'
+		];
+	
+		return syntaxArray.join("\n");
+	}
+
+    public parseWiki(): void {
+		// TODO: Implement xml parse and possibly get of pages
+
+		try {
+			const parsedSyntaxes: ParsedSyntax[] = this.parsePageIntoSyntaxes(
+				"select",
+				selectText
+			);
+			console.table(parsedSyntaxes);
+			console.log(this.consolidateSyntaxes("select",parsedSyntaxes));
+		} catch (error) {
+			console.log(error);
+		}
+		
+	}
+}
