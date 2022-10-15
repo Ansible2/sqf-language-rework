024d9f69621944a3be70360124d0e3c5661237d0
Ansible2 <afoshark@yahoo.com>
2022-10-14 22:28:45 -0600

    Added more kiska parser regexs

--- Parsers/KISKA Parser/KiskaParser.ts
+++ Parsers/KISKA Parser/KiskaParser.ts
@@ -1,2 +1,17 @@
 // select header comment
 // /(?<=\/\* \-+\n)([\s\S]*?)(?=\n+\-+ \*\/\n)/i
+
+// select description 
+// /(?<=description:\n*)([\s\S]*)(?=Parameters:)/i
+
+// select parameters
+// /(?<=parameters:\n*)([\s\S]*)(?=Returns:)/i
+
+// select indiviual parameters (Requires that "<END>" be place at the end of the parameters string) 
+// /(?<=^(\t| {0,4}))(\d:)([\s\S]*?)(?=(^(\t| {0,4}))(\d:)|<END>)/gim
+
+// select returns
+// /(?<=Returns:\n*)([\s\S]*)(?=Examples:)/i
+
+// select examples
+// /(?<=Example\w*:\n*)([\s\S]*)(?=author[\w\W]*?:)/i
