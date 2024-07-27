cc19e12464477352a2c9517b88f55d9b0d0cf426
Ansible2 <afoshark@yahoo.com>
2024-07-27 16:49:55 -0600

    Started on parameter parsing

--- Parsers/KISKA Parser/KiskaParserV2.ts
+++ Parsers/KISKA Parser/KiskaParserV2.ts
@@ -1,4 +1,4 @@
-import { SQFItemConfig } from "../../configuration/grammars/sqf.namespace";
+import { SQFGrammarType, SQFItemConfig } from "../../configuration/grammars/sqf.namespace";
 import {
     DocParser,
     getParserSecrets,
@@ -13,6 +13,7 @@ import fs from "fs-extra";
 
 interface UnparsedKiskaPage extends UnparsedItem {
     fileName: string;
+    documentationLink: string;
 }
 
 export class KiskaParserV2 implements DocParser {
@@ -21,6 +22,10 @@ export class KiskaParserV2 implements DocParser {
 
     constructor() {}
 
+    private getDocumentationLink(entry: IGithubTreeEntry): string {
+        return `https://github.com/Ansible2/Kiska-Function-Library/blob/${entry.sha}/${entry.path}`;
+    }
+
     async getLatestSeedFile(): Promise<string> {
         const secrets = await getParserSecrets();
         const REQUEST_OPTIONS = Object.freeze({
@@ -28,31 +33,12 @@ export class KiskaParserV2 implements DocParser {
         });
 
         console.log("Fetching git tree...");
-        const gitTreeUrl =
+        const REPO_TREE_URL =
             "https://api.github.com/repos/Ansible2/Kiska-Function-Library/git/trees/master?recursive=1";
-        const gitTree = await fetch(gitTreeUrl, REQUEST_OPTIONS).then(
+        const gitTree = await fetch(REPO_TREE_URL, REQUEST_OPTIONS).then(
             (response) => response.json() as unknown as IGithubTreeResponse
         );
         console.log("Completed tree fetch with", gitTree.tree.length, "items");
-
-        let currentRequests: Promise<UnparsedKiskaPage | null>[] = [];
-        const unparsedPages: UnparsedKiskaPage[] = [];
-        const waitForCurrentRequestsToComplete = async (index: number) => {
-            console.log(
-                "Waiting for",
-                currentRequests.length,
-                "requests to finish at index",
-                index
-            );
-
-            const responses = await Promise.all(currentRequests);
-            responses.forEach((response) => {
-                if (response) {
-                    unparsedPages.push(response);
-                }
-            });
-            currentRequests = [];
-        };
 
         const getUnparsedKiskaPage = async (
             entry: IGithubTreeEntry
@@ -71,20 +57,37 @@ export class KiskaParserV2 implements DocParser {
             return {
                 text: decoded,
                 fileName,
+                documentationLink: this.getDocumentationLink(entry),
             };
         };
 
-        let index = 0;
+        let currentRequests: Promise<UnparsedKiskaPage | null>[] = [];
+        const unparsedPages: UnparsedKiskaPage[] = [];
+        let index = -1;
+        const maxIndex = gitTree.tree.length - 1;
         for (const entry of gitTree.tree) {
-            currentRequests.push(getUnparsedKiskaPage(entry));
-            if (currentRequests.length >= this.MAX_NUMBER_OF_CONCURRENT_REQUESTS) {
-                await waitForCurrentRequestsToComplete(index);
-            }
             index++;
-        }
-        
-        if (currentRequests.length) {
-            await waitForCurrentRequestsToComplete(gitTree.tree.length - 1);
+            currentRequests.push(getUnparsedKiskaPage(entry));
+
+            const waitForCurrentRequestsToComplete =
+                currentRequests.length >= this.MAX_NUMBER_OF_CONCURRENT_REQUESTS ||
+                index === maxIndex;
+            if (waitForCurrentRequestsToComplete) {
+                console.log(
+                    "Waiting for",
+                    currentRequests.length,
+                    "requests to finish at index",
+                    index
+                );
+
+                const responses = await Promise.all(currentRequests);
+                responses.forEach((response) => {
+                    if (response) {
+                        unparsedPages.push(response);
+                    }
+                });
+                currentRequests = [];
+            }
         }
         console.log("Total number of files fetched ->", unparsedPages.length);
 
@@ -111,13 +114,57 @@ export class KiskaParserV2 implements DocParser {
     }
 
     private parseKiskaPage(unparsedPage: UnparsedKiskaPage): SQFItemConfig {
-        return {
-            documentation: {},
+        const page = unparsedPage.text;
+        const headerRegexMatch = page.match(
+            /(?<=\/\* \-+\r*\n+)([\s\S]*?)(?=\r*\n+\-+ \*\/\r*\n+)/i
+        );
+
+        if (!headerRegexMatch) {
+            throw new Error("Could not find header comment");
+        }
+
+        const headerComment = headerRegexMatch[0];
+        const functionName = headerComment.match(/(?<=function:\s*)\b.*/i)?.at(0);
+        if (!functionName) {
+            throw new Error("Could not find function name");
+        }
+
+        const itemConfig: SQFItemConfig = {
+            documentation: {
+                documentationLink: unparsedPage.documentationLink,
+                syntaxes: [],
+                examples: [],
+            },
             configuration: {
-                label: "",
-                grammarType: "",
+                label: functionName,
+                grammarType: SQFGrammarType.Function,
             },
         };
+
+        const descriptionMatch = headerComment.match(
+            /(?<=description:\r*\n*)([\s\S]*?)(?=Parameters:)/i
+        );
+        if (descriptionMatch) {
+            // ([\n\r\t]+| {2,}) decent replace regex but still leaves double spaces
+            // should seperate the ( {2,}) into one afterthe first replace(?)
+            const description = descriptionMatch[0].replace(/[\n\r\t]+/gi, " ");
+            itemConfig.documentation.description = description.replace(/ {2,}/gi, " ");
+        }
+
+        const parametersMatch = headerComment.match(
+            /(?<=parameters:\r*\n*)([\s\S]*?)(?=(Examples:|returns:))/i
+        );
+        if (parametersMatch) {
+            let fullParameterSection = parametersMatch[0];
+            fullParameterSection += "<END>";
+
+            const individualParametersMatch = fullParameterSection.match(
+                /(?<=^(\t| {0,4}))(\d:)([\s\S]*?)(?=(^(\t| {0,4}))(\d:)|<END>)/gim
+            );
+            // TODO: parse parameters
+        }
+
+        return itemConfig;
     }
 
     getOutputFileName(): string {
