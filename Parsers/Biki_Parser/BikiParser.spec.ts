503f316503b54d8b913ffa278c1dc9c5c5cb4a0a
Ansible2 <afoshark@yahoo.com>
2024-08-18 12:12:24 -0600

    Added more test pages and started on more unit tests

--- Parsers/Biki_Parser/BikiParser.spec.ts
+++ Parsers/Biki_Parser/BikiParser.spec.ts
@@ -24,6 +24,7 @@ describe("BikiCommandsParser", () => {
 
     describe.concurrent("convertItemConfigs", () => {
         for (const [name, testStatics] of Object.entries(BikiTestPages)) {
+            // if (name !== 'drawIcon3D') continue;
             it(`should convert ${name} into a config`, async () => {
                 const page: UnparsedBikiPage = {
                     title: name,
